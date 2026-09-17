61b8fef1c3fbdb092050e91fc43370acecd946bc
omi0325 <125053550@upq.edu.mx>
2026-09-17 17:18:42 -0600

    php
    trabajamos php y con html

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+ echo "Hola Fantasy";
+ ?>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1 {
+            color: #4bffc0;
+        }
+        div{
+            background: #a623f1;
+        }
+    </style>
+ </head>
+ <body>
+    <h1>hola crayola</h1>
+    <h2>hola crayola</h2>
+    <h3>hola crayola</h3>
+    <h4>hola crayola</h4>
+    <h5>hola crayola</h5>
+    <h6>hola crayola</h6>
+    <div><h2>titulo del parrafo</h2></div>
+    <div>Este es un parrafo</div>
+ </body>
+ </html>
